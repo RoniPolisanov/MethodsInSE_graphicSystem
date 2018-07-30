5d9815ae599a9d8cc535ed2fef8f8a1ba4427c44
--replace-all <haim1321@gmail.com>
2018-07-30 17:08:06 +0300

    TextBox upload - stopped at cpp

--- TextBox.cpp
+++ TextBox.cpp
@@ -1,9 +1,23 @@
+#include <iostream>
 #include "TextBox.h"
 
+using namespace std;
 
-
-
-
-TextBox::~TextBox()
+//init TextBox
+TextBox::TextBox(int width)
 {
+    this->width = width;
+    this->height = 1;
+    this->border = BorderType::Single;
+    this->showed = true;
+}
+
+//draw textbox method
+void TextBox::draw(Graphics g, int x, int y, size_t z)
+{
+    g.setBackground(this->background);
+    g.setForeground(this->foreground);
+    g.write(x, y, this->GetValue());
+    g.setForeground(Color::White);
+    g.setBackground(Color::Black);
 }
