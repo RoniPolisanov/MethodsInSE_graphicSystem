1b5741469ad35b4e5b5dca1d7b0d06bd2afbfd87
RoniPolisanov <yyyroni@gmail.com>
2018-07-31 23:38:22 +0300

    Draw fixing

--- NumericBox.cpp
+++ NumericBox.cpp
@@ -14,7 +14,36 @@ NumericBox::NumericBox(int width, int min, int max) {
 void NumericBox::draw(Graphics g, int x, int y, size_t z) {
 	// Setting Foreground and Background
 	this->graphics.setBackground(this->background);
-	this->graphics.setForeground(this->foregrou
+	this->graphics.setForeground(this->foreground);
+
+	// Drawing the MINUS "button"
+	this->graphics.write(x, y + 1, "(-)");
+
+	// Drawing the NumericBox "frame"
+	this->graphics.write(x + 3, y, "\xC9");
+	for (int i = 0; i < this->getWidth() - 8; i++)
+		graphics.write("\xCD");
+
+	this->graphics.write("\xBB\n");
+	this->graphics.write(x + 3, y + 1, "\xBA");
+	this->graphics.write(x + this->getWidth() - 4, y + 1, "\xBA");
+	this->graphics.write(x + 3, y + 2, "\xC8");
+
+	for (int i = 0; i < this->getWidth() - 8; i++)
+		this->graphics.write("\xCD");
+
+	this->graphics.write("\xBC");
+
+	// Conveting the number to string and drawing it
+	string val = to_string(this->value);
+	this->graphics.write(x + this->getWidth() / 2, y + 1, val);
+
+	// Drawing the PLUS "button"
+	this->graphics.write(x + this->getWidth() - 3, y + 1, "(+)");
+
+	// Setting Foreground and Background to default
+	this->graphics.setForeground(Color::White);
+	this->graphics.setBackground(Color::Black);
 }
 
 // Handling Mouse click event and operating it
