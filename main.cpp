99c1c26b512d9bcabe925d8a5144d72e5ec75fe7
ReutLeib <leibreut@mail.com>
2018-07-31 16:57:01 +0300

    try to use Label

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Authors: Haim Elbaz ~ Roni Polisanov ~ Reut Leib
+#include <iostream>
+#include "Control.h"
+#include "Graphics.h"
+#include "EventEngine.h"
+#include "Panel.h"
+#include "Label.h"
+
+using namespace std;
+
+int main() {
+
+  Label labelName("name");
+  return 1;
+}
+
+
