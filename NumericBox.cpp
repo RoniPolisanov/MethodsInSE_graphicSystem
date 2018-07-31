e6bfa4253bb61517f2ebbee45c092203e60c116f
RoniPolisanov <yyyroni@gmail.com>
2018-07-31 10:38:05 +0300

    Foreground, Background and cursor changes

--- NumericBox.cpp
+++ NumericBox.cpp
@@ -12,9 +12,14 @@ NumericBox::NumericBox(int width, int min, int max) {
 
 // Drawing the NumericBox
 void NumericBox::draw(Graphics g, int x, int y, size_t z) {
+	// Setting Foreground and Background
+	this->graphics.setBackground(this->background);
+	this->graphics.setForeground(this->foregrou
 }
 
 // Handling Mouse click event and operating it
 void NumericBox::mousePressed(int x, int y, DWORD button) {
+	this->graphics.setCursorVisibility(true);
+	this->graphics.moveTo(x, y);
 
 }
