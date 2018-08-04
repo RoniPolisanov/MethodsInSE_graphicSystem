e81254398ced2cfed381b9321a0feb8b0127bc3d
RoniPolisanov <yyyroni@gmail.com>
2018-08-04 18:34:14 +0300

    MessageBox Header fixed

--- Messagebox.h
+++ Messagebox.h
@@ -1,12 +1,24 @@
 #pragma once
 #include "Control.h"
+#include "Button.h"
 
 class Messagebox : public Control {
 
 private:
-
+	string title, text;
+	vector <Button*> buttons;
 
 public:
-  Messagebox() {}
+	Messagebox(int, int);
+	void SetTitle(string title)	{ this->title = title; }
+	string GetTitle()			{ return this->title; }
+	void SetText(string text)	{ this->text = text; }
+	string GetText()			{ return this->text; }
+	// No focus according to task definition
+	bool canGetFocus()			{ return false; }		
+	void draw(Graphics, int, int, size_t);
+	void keyDown(int, char) {}
+	void mousePressed(int, int, DWORD);
+	void addButton(Button&, int, int);
 };
 
