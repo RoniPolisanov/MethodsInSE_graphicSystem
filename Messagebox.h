7cf3059b8a1a8fac8621517852bb0df6f0f15d42
ReutLeib <leibreut@mail.com>
2018-08-05 11:00:24 +0300

    add methods to messageBox

--- Messagebox.h
+++ Messagebox.h
@@ -4,9 +4,20 @@
 class Messagebox : public Control {
 
 private:
-
+  string title = "";
+  string text = "";
 
 public:
   Messagebox() {}
+  Messagebox(int, int);
+  void SetTitle(string title);
+  string GetTitle();
+  void SetText(string text);
+  string GetText();
+  bool canGetFocus(); 
+  void draw(Graphics, int, int, size_t);
+  void keyDown(int, char) {}
+  void mousePressed(int, int, DWORD);
+  void addButton(Button&, int, int);
 };
 
