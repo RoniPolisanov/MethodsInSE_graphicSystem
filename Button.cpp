6e06ff7fd3f12d34f946987e13c727919d5720db
ReutLeib <leibreut@mail.com>
2018-08-06 12:33:30 +0300

    fixed code in Button

--- Button.cpp
+++ Button.cpp
@@ -9,25 +9,10 @@ Button::Button(int width) {
   this->value = "Submit";
 }
 
-void Button::SetValue(string value){ 
-  this->value = value; 
-} 
-
-string Button::GetValue(){ 
-  return this->value;
-} 
-
-bool Button::canGetFocus(){ 
-  return false;
-} 
-
-void Button::keyDown(int keyCode, char character){ 
-  this->graphics.clearScreen();
-} 
-
 void Button::draw(Graphics g, int x, int y, size_t z) {
   graphics.setBackground(this->background);
   graphics.setForeground(this->foreground);
   drawBackground();
   graphics.write(x, y, this->GetValue());
 }
+
