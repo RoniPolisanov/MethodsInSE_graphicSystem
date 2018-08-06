bc0b1162ee663756c65d6e8d3a25ab7c17be3cf0
ReutLeib <leibreut@mail.com>
2018-08-06 12:26:47 +0300

    final comboBox

--- ComboBox.cpp
+++ ComboBox.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include "ComboBox.h"
+
+using namespace std;
 
 ComboBox::ComboBox(int width, vector<string> options) {
   this->width = width;
@@ -7,27 +10,37 @@ ComboBox::ComboBox(int width, vector<string> options) {
   this->showed = true;
 }
 
-void ComboBox::mousePressed(int x, int y, DWORD button) {
-  int top = this->getTop();
-  int left = this->getLeft();
-  vector<string> options = this->options;
+void ComboBox::draw(Graphics g, int x, int y, size_t z) {
+  graphics.setBackground(this->background);
+  graphics.setForeground(this->foreground);
+  drawBackground();
 
   switch (this->isOpened) {
-  case false:
-    this->height += options.size();
-    this->isOpened = true;
+  case false: //combobox closed
+    graphics.write(x + this->width - 3, y, "\xBA"); // ||  border symbol - ascii table
+    graphics.write(x + this->width - 1, y, "\xFB");
+    graphics.write(x + 1, y, this->options[this->GetSelectedIndex()]);
     break;
-  case true:
-    for (int k = 0; k < options.size(); ++k) {
-      if (y == top + k + 1) {
-        this->selected = k;
-        this->height -= options.size();
-        this->isOpened = false;
-        break;
+
+  case true:  //combobox opened
+    graphics.write(x + this->width - 3, y, "\xBA"); // || border symbol - ascii table
+    graphics.write(x + this->width - 1, y, "\xFB");
+    graphics.write(x + 1, y, this->options[this->GetSelectedIndex()]); //write the text of selected option 
+
+    for (int i = 0; i < options.size(); ++i) {
+      //handle the graphics of the selected option
+      if (i == GetSelectedIndex()) {
+        graphics.setBackground(Color::Blue);
+        graphics.write(this->getLeft() + 1, this->getTop() + i + 1, this->options[i]);
+        graphics.setBackground(this->background);
       }
+      else
+        graphics.write(this->getLeft() + 1, this->getTop() + i + 1, this->options[i]);
     }
   }
-};
+  graphics.setForeground(Color::White);
+  graphics.setBackground(Color::Black);
+}
 
 void ComboBox::keyDown(int keyCode, char character) {
   int position = this->getTop() + this->GetSelectedIndex() + 1;
@@ -60,3 +73,26 @@ void ComboBox::keyDown(int keyCode, char character) {
     this->height -= options.size();
   this->isOpened = false;
 }
+
+void ComboBox::mousePressed(int x, int y, DWORD button) {
+  int top = this->getTop();
+  int left = this->getLeft();
+  vector<string> options = this->options;
+
+  switch (this->isOpened) {
+  case false:
+    this->height += options.size();
+    this->isOpened = true;
+    break;
+  case true:
+    for (int k = 0; k < options.size(); ++k) {
+      if (y == top + k + 1) {
+        this->selected = k;
+        this->height -= options.size();
+        this->isOpened = false;
+        break;
+      }
+    }
+  }
+};
+
