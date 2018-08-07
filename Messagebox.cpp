6c67697235ef50a97d898060d173820765faf8ec
ReutLeib <leibreut@mail.com>
2018-08-07 11:51:40 +0300

    fixed messageBox

--- Messagebox.cpp
+++ Messagebox.cpp
@@ -13,22 +13,20 @@ Messagebox::Messagebox(int width, int height) {
 // Drawing the MessageBox
 void Messagebox::draw(Graphics g, int x, int y, size_t z) {
 	// Setting Foreground and Background
-	this->graphics.setBackground(this->background);
-	this->graphics.setForeground(this->foreground);
-
-	this->drawBackground();
-	this->graphics.write(x, y, this->GetTitle());
-	this->graphics.write(x, y + 2, this->GetText());
+	graphics.setBackground(this->background);
+	graphics.setForeground(this->foreground);
+	drawBackground();
+	graphics.write(x, y, this->GetTitle());
+	graphics.write(x, y + 2, this->GetText());
 
 	for (int i = 0; i < this->buttons.size(); ++i) {
 		if (buttons[i]->getShowed() == true) {
-			buttons[i]->draw(this->graphics, buttons[i]->getLeft(), buttons[i]->getTop(), 0);
+			buttons[i]->draw(graphics, buttons[i]->getLeft(), buttons[i]->getTop(), 0);
 			buttons[i]->drawBorder(buttons[i]->getBorder());
 		}
 	}
 }
 
-// Mouse click event
 void Messagebox::mousePressed(int x, int y, DWORD button) {
 	for (int i = 0; i < this->buttons.size(); ++i) {
 		if (isInside(x, y, this->buttons[i]->getLeft(), this->buttons[i]->getTop(), this->buttons[i]->getWidth(), this->buttons[i]->getHeight())){
@@ -38,10 +36,9 @@ void Messagebox::mousePressed(int x, int y, DWORD button) {
 	}
 };
 
-// Add button to display
+//add button to display
 void Messagebox::addButton(Button &button, int left, int top) {
 	this->buttons.push_back(&button);
 	button.setLeft(this->getLeft() + left);
 	button.setTop(this->getTop() + top);
 }
-
--- Messagebox.h
+++ Messagebox.h
@@ -7,18 +7,17 @@ class Messagebox : public Control {
 private:
   string title = "";
   string text = "";
+  vector <Button*> buttons;
 
 public:
-  Messagebox()            {}
-  void keyDown(int, char) {}
-  bool canGetFocus(); 
   Messagebox(int, int);
-  void SetTitle(string title);
-  void SetText(string text);
-  string GetTitle();
-  string GetText();
+  void SetTitle(string title) { this->title = title; }
+  string GetTitle()     { return this->title; }
+  void SetText(string text) { this->text = text; }
+  string GetText()      { return this->text; }
+  bool canGetFocus()      { return false; }   //no focus according to task definition
   void draw(Graphics, int, int, size_t);
+  void keyDown(int, char) {}
   void mousePressed(int, int, DWORD);
   void addButton(Button&, int, int);
 };
-
